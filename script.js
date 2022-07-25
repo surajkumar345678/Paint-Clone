4f6af077c97b419bf048ad735315806f7cd9b5e4
Suraj Kumar <kumarsuraj345678@gmail.com>
2022-07-25 13:09:03 +0530

    canvas setup

--- script.js
+++ script.js
@@ -13,7 +13,9 @@ const downloadBtn = document.getElementById('download');
 const { body } = document;
 
 // Global Variables
-
+const canvas = document.createElement('canvas');
+canvas.id = 'canvas';
+const context = canvas.getContext('2d');
 let currentSize = 10;
 let bucketColor = '#FFFFFF';
 let currentColor = '#A51DAB';
@@ -63,10 +65,10 @@ let currentColor = '#A51DAB';
 
 // Create Canvas
 function createCanvas() {
-  // canvas.width = ;
-  // canvas.height = ;
-  // context.fillStyle = ;
-  // context.fillRect();
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight-50;
+  context.fillStyle = bucketColor;
+  context.fillRect(0,0,canvas.width,canvas.height);
   body.appendChild(canvas);
 
 }
@@ -124,11 +126,11 @@ canvas.addEventListener('mousedown', (event) => {
   isMouseDown = true;
   const currentPosition = getMousePosition(event);
   console.log('mouse is clicked', currentPosition);
-//   context.moveTo(currentPosition.x, currentPosition.y);
-//   context.beginPath();
-//   context.lineWidth = currentSize;
-//   context.lineCap = 'round';
-//   context.strokeStyle = currentColor;
+  //   context.moveTo(currentPosition.x, currentPosition.y);
+  //   context.beginPath();
+  //   context.lineWidth = currentSize;
+  //   context.lineCap = 'round';
+  //   context.strokeStyle = currentColor;
 });
 
 // Mouse Move
@@ -136,17 +138,17 @@ canvas.addEventListener('mousemove', (event) => {
   if (isMouseDown) {
     const currentPosition = getMousePosition(event);
     console.log('mouse is moving', currentPosition);
-  //   context.lineTo(currentPosition.x, currentPosition.y);
-  //   context.stroke();
-  //   storeDrawn(
-  //     currentPosition.x,
-  //     currentPosition.y,
-  //     currentSize,
-  //     currentColor,
-  //     isEraser,
-  //   );
-  // } else {
-  //   storeDrawn(undefined);
+    //   context.lineTo(currentPosition.x, currentPosition.y);
+    //   context.stroke();
+    //   storeDrawn(
+    //     currentPosition.x,
+    //     currentPosition.y,
+    //     currentSize,
+    //     currentColor,
+    //     isEraser,
+    //   );
+    // } else {
+    //   storeDrawn(undefined);
   }
 });
 
